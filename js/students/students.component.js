8a5a6c736a70971914394db17a0e4aeb79d00071
Mathew Brown <matt@mattbrown.com>
2017-03-03 13:32:15 -0700

    Add Students Component

--- js/students/students.component.js
+++ js/students/students.component.js
@@ -8,12 +8,16 @@
     });
 
 
-    function studentsController() {
+    studentsController.$inject = ['$http']
+
+    function studentsController($http) {
       const vm = this;
 
       vm.$onInit = function() {
-        console.log('Students Init');
-      }
+        $http.get('/js/students/students.json').then(function (response) {
+          vm.students = response.data.students;
+        });      }
+        vm.searchInput = '';
 
 
     }
